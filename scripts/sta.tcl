b16f646ec4d51a987bafef92a70a0889acc3547c
licit <licit@licit-charizard.com>
2023-04-26 15:52:31 -0600

    Cambios finales ya tira todo, falta agregar file y modificar ff.txt

--- scripts/sta.tcl
+++ scripts/sta.tcl
@@ -69,50 +69,62 @@ puts "min_report_end"
 #}
 #close $file
 
-# Abrir el archivo de texto que contiene la lista de tipos de flip-flops
-set file [open "tipos_ff.txt" r]
-# Leer la lista de tipos de flip-flops y dividirla en una lista de elementos
-set tipos_ff [split [read $file] "\n"]
-# Cerrar el archivo
-close $file
+#if {$::env(CURRENT_STEP) != "synthesis"} {
 
-puts "REPORTE_ANA"
-puts "\n==========================================================================="
-puts "report_checks -path_delay FF MIN (Hold)"
-puts "============================================================================"
-# Iterar sobre cada tipo de flip-flop en la lista
-foreach tipo $tipos_ff {
-    # Obtener una lista de celdas que coinciden con el tipo de flip-flop actual
-    #set lista_ffs [get_cells -filter "ref_name == $tipo"]
-    set lista_ffs [get_cells -regexp "$tipo"]
+    # Abrir el archivo de texto que contiene la lista de tipos de flip-flops
+    set file [open "tipos_ff.txt" r]
+    # Leer la lista de tipos de flip-flops y dividirla en una lista de elementos
+    set tipos_ff [split [read $file] "\n"]
+    # Cerrar el archivo
+    close $file
 
-    # Iterar sobre cada celda en la lista y generar un informe de temporización
-    foreach ff $lista_ffs {
-        #report_checks -to [get_name $ff] -format full_clock_expanded
-         set cell_name [get_name $ff]
-        if {[string length $cell_name] > 0} {
-            report_checks -to $cell_name -fields {slew cap input nets fanout} -format full_clock_expanded
-            puts $cell_name
+    puts "REPORTE_ANA"
+    puts $tipos_ff
+    puts "\n==========================================================================="
+    puts "report_checks -path_delay FF MIN (Hold)"
+    puts "============================================================================"
+    # Iterar sobre cada tipo de flip-flop en la lista
+    foreach tipo $tipos_ff {
+        puts $tipo
+        # Obtener una lista de celdas que coinciden con el tipo de flip-flop actual
+        if {[string length $tipo] > 0} {
+            set lista_ffs [get_cells -filter "ref_name == ${tipo}"]
         } else {
-            puts "Cell with name $cell_name not found"
+            continue
         }
-    }
+        # Iterar sobre cada celda en la lista y generar un informe de temporización
+        puts $lista_ffs
+        foreach ff $lista_ffs {
+            puts $ff
+            #report_checks -to [get_name $ff] -format full_clock_expanded
+            set cell_name [get_name $ff]
+            if {[string length $cell_name] > 0} {
+                report_checks -path_delay max -to $cell_name -fields {slew cap input nets fanout} -format full_clock_expanded
 
-
-
-if 0 {foreach ff $lista_ffs {
-    # Check if the current cell is a flip-flop or not
-    if {[string match "*$tipo*" [get_attribute $ff CELL_TYPE]]} {
-        # The current cell is a flip-flop of the desired type, so report on it
-        set cell_name [get_name $ff]
-        if {[string length $cell_name] > 0} {
-            report_checks -to $cell_name -format full_clock_expanded
-        } else {
-            puts "Cell with name $cell_name not found"
+                puts $cell_name
+            } else {
+                puts "Cell with name $cell_name not found"
+            }
         }
-    }
-}}
+
+
+
+    if 0 {foreach ff $lista_ffs {
+        # Check if the current cell is a flip-flop or not
+        if {[string match "*$tipo*" [get_attribute $ff CELL_TYPE]]} {
+            # The current cell is a flip-flop of the desired type, so report on it
+            set cell_name [get_name $ff]
+            if {[string length $cell_name] > 0} {
+                report_checks -to $cell_name -format full_clock_expanded
+            } else {
+                puts "Cell with name $cell_name not found"
+            }
+        }
+    }}
 }
+    
+#}
+
 
 #report_checks -path_delay min -fields {slew cap input nets fanout} -format full_clock_expanded -group_count 5
 puts "REPORTE_ANA_end"
